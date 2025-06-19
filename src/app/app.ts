0acaa33d288c3571c685fc37f359ebe4fa49ac4f
Eric Albert Alonso <dulothings@gmail.com>
2025-06-19 11:03:29 +0200

    Header ended, footer ended, start the presentation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './header/header';
-import { DarkmodeButton } from './darkmode-button/darkmode-button'
-import { Footer } from './footer/footer'
 import { DeviceDetectorService } from 'ngx-device-detector';
+
+import { DarkmodeButton } from './darkmode-button/darkmode-button'
+import { Header } from './header/header';
+import { Presentation } from './body/presentation/presentation';
+import { Footer } from './footer/footer'
 
 @Component({
   selector: 'app-root',
   standalone: true ,
-  imports: [RouterOutlet,Header,DarkmodeButton,Footer],
+  imports: [RouterOutlet,Header,DarkmodeButton,Footer,Presentation],
   providers: [DeviceDetectorService],
   templateUrl: './app.html',
   styleUrl: './app.scss'
