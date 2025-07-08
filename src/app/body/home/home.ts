3085a247e1c8eae329017712963a6dcac0f98a1d
Eric Albert Alonso <dulothings@gmail.com>
2025-07-08 10:18:58 +0200

    Body hecho

--- src/app/body/home/home.ts
+++ src/app/body/home/home.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { Experience } from '../experience/experience';
+import { Proyects } from '../proyects/proyects';
+import { About } from '../about/about';
 
 @Component({
   selector: 'home',
   standalone: true ,
-  imports: [],
+  imports: [Experience,About,Proyects],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
