9f3d1a4b912c89e9ea9636bc8059356822287983
Eric Albert Alonso <dulothings@gmail.com>
2025-07-09 10:39:12 +0200

    About terminado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { Home } from './body/home/home';
+import { Presentation } from './body/presentation/presentation';
 import { About } from './body/about/about'
 import { Experience } from './body/experience/experience';
 import { Proyects } from './body/proyects/proyects';
 
 export const routes: Routes = [
-    { path:'', component:Home, pathMatch: 'full'},
+    { path:'', component:Presentation, pathMatch: 'full'},
     { path:'about', component:About },
     { path:'experience', component:Experience },
     { path:'proyects', component:Proyects }
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { DeviceDetectorService } from 'ngx-device-detector';
-
-import { DarkmodeButton } from './darkmode-button/darkmode-button'
 import { Header } from './header/header';
-import { Presentation } from './body/presentation/presentation';
-import { Footer } from './footer/footer'
-
 
 @Component({
   selector: 'app-root',
   standalone: true ,
-  imports: [RouterOutlet, Header, DarkmodeButton, Footer, Presentation],
+  imports: [RouterOutlet, Header],
   providers: [DeviceDetectorService],
   templateUrl: './app.html',
   styleUrl: './app.scss'
