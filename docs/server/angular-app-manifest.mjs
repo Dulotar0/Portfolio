a5266ef0591cbb7b03486e8d1e826a9e491f8e94
Eric Albert Alonso <dulothings@gmail.com>
2025-07-17 11:31:47 +0200

    Lo que faltaba antes

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -24,12 +24,12 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 3613, hash: '70bab63bbbb4c2a2f98e00a719f6e658fd147a9f679047cd15440a35aa26aa6c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 3409, hash: '6e5002295ea4321a83c036c5e687ba9b63f012d2d3ff47a6c698de9be895b230', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'about/index.html': {size: 18187, hash: '0f20f8bfa2196e031968a53a5326a8ae1d794fa4398d56c510bc3935055e1526', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
-    'proyects/index.html': {size: 20184, hash: '2ce7b55becc1574f1a6e73a00dbf5165fff77463758b6a64e5dc64ba779f0dac', text: () => import('./assets-chunks/proyects_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 15019, hash: '7857d8becb6d1c3a2b5da04db1707c93f4b540cc830d62c5a887ce1389496116', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'experience/index.html': {size: 17675, hash: 'ed1b8194fe92277a480aa617d0d68e05708dec9206ee06a06f81bfc0bc11b820', text: () => import('./assets-chunks/experience_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 3613, hash: '61d107acfb11ff6a26eed5620158e7ac870c7df3b8311180bea871c38a92b998', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 3409, hash: '24a5695617608a40084c42503eec80d4c2889bae36df8bb48f34253f035218a3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'about/index.html': {size: 18271, hash: '0a6f63602a61dbb17745451a38833d3cd916145888b8f7ba0da72119de4b7c7e', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 15103, hash: 'f97c010a407ce52279abe42fec8ec31d625b2a06f34fe6e9f617d574861c3c20', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'experience/index.html': {size: 17759, hash: '7427037a527982803cb47de3243ead5d182ffa62ed40d14da1b8c41df9b48348', text: () => import('./assets-chunks/experience_index_html.mjs').then(m => m.default)},
+    'proyects/index.html': {size: 20268, hash: '4094c828adb54757ed6cc3e21947ad58e26ed719bf80da989b21e653893de20f', text: () => import('./assets-chunks/proyects_index_html.mjs').then(m => m.default)},
     'styles-LICQ23OS.css': {size: 1882, hash: 'K6y8lhRILes', text: () => import('./assets-chunks/styles-LICQ23OS_css.mjs').then(m => m.default)}
   },
 };
